18ac1ac15eb179d60818d3fca86f9d02ff5fa9f8
sarthaksuperb <74700353+sarthaksuperb@users.noreply.github.com>
2021-02-10 12:43:58 +0530

    Add files via upload

---
+++ KeeperApp/src/components/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  function addNote(note) {
+    setNotes((prevValue) => {
+      return [...prevValue, note];
+    });
+  }
+  function deleteNote(id){
+    setNotes((prevValue)=>{
+      return prevValue.filter((noteItem,index)=>{
+        return index!==id
+      });
+    });
+  }
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem,index) => {
+        return <Note id={index} key={index} title={noteItem.title} content={noteItem.content} onDelete={deleteNote} />;
+      })}
+      {/* <Note key={1} title="Note title" content="Note content" /> */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
